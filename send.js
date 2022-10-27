36a9f60612a7b80b4479bcac374a0b44cb195f4e
kan.kosal <kan.kosal@sabay.com>
2022-10-27 17:28:10 +0700

    clean code

--- send.js
+++ send.js
@@ -1,46 +1,46 @@
-var amqp = require("amqplib/callback_api");
+var amqp = require('amqplib/callback_api')
 
-amqp.connect("amqp://localhost", function (error0, connection) {
+amqp.connect('amqp://localhost', function (error0, connection) {
   if (error0) {
-    throw error0;
+    throw error0
   }
   connection.createChannel(function (error1, channel) {
     if (error1) {
-      throw error1;
+      throw error1
     }
 
-    var queue = "process-video-flavor-queue";
+    var queue = 'process-video-flavor-queue'
 
     var msg2 = JSON.stringify({
-      "id":"635756a914d0fc0030821bce",
-      "duration":853,
-      "codec":"avc1",
-      "format":"mp4",
-      "uploadStatus":"success",
-      "flavors":[
-         {
-            "flavor":"Source",
-            "fileName":"0_b5l1t09g",
-            "format":"mp4",
-            "codec":"avc1",
-            "dimension":"1280 x 720",
-            "bitrate":133325,
-            "size":28251,
-            "uploadStatus":"success"
-         }
-      ]
-   });
+      id: '635756a914d0fc0030821bce',
+      duration: 853,
+      codec: 'avc1',
+      format: 'mp4',
+      uploadStatus: 'success',
+      flavors: [
+        {
+          flavor: 'Source',
+          fileName: '0_b5l1t09g',
+          format: 'mp4',
+          codec: 'avc1',
+          dimension: '1280 x 720',
+          bitrate: 133325,
+          size: 28251,
+          uploadStatus: 'success',
+        },
+      ],
+    })
 
     channel.assertQueue(queue, {
       durable: true,
-    });
+    })
     channel.sendToQueue(queue, Buffer.from(msg2), {
       persistent: true,
-    });
-    console.log(" [x] Sent ===================================>");
-  });
+    })
+    console.log(' [x] Sent ===================================>')
+  })
   setTimeout(function () {
-    connection.close();
-    process.exit(0);
-  }, 500);
-});
+    connection.close()
+    process.exit(0)
+  }, 500)
+})
--- sendExchange.js
+++ sendExchange.js
@@ -1,63 +1,28 @@
-const amqp = require("amqplib");
-async function fanoutExchange(){
-    try{
-        const rabbitmqUrl = "amqp://localhost";
-        const connection = await amqp.connect(rabbitmqUrl);
-        const exchange = "cashier.transfer.sg.to.mysabay.user";
-        // const exchange = "mysabay.user.purchase.ak.gold";
-        const exchangeType = "fanout";
-        const routingKey = "";
-        const options = {};
-        const payload = {
-            txn_hash:
-              "f29b63bff9332cbd1de7f938a4561551973b5f50e5fdcbcb8e26d98a541d1592"
-          };
-        let channel = await connection.createChannel();
-        await channel.assertExchange(exchange, exchangeType, options);
-        channel.publish(
-            exchange,
-            routingKey,
-            Buffer.from(JSON.stringify(payload)),
-            options
-        );
-    }catch(error){
-        console.error(error)
+const amqp = require('amqplib')
+async function fanoutExchange() {
+  try {
+    const rabbitmqUrl = 'amqp://localhost'
+    const connection = await amqp.connect(rabbitmqUrl)
+    const exchange = 'cashier.transfer.sg.to.mysabay.user'
+    // const exchange = "mysabay.user.purchase.ak.gold";
+    const exchangeType = 'fanout'
+    const routingKey = ''
+    const options = {}
+    const payload = {
+      txn_hash:
+        'f29b63bff9332cbd1de7f938a4561551973b5f50e5fdcbcb8e26d98a541d1592',
     }
+    let channel = await connection.createChannel()
+    await channel.assertExchange(exchange, exchangeType, options)
+    channel.publish(
+      exchange,
+      routingKey,
+      Buffer.from(JSON.stringify(payload)),
+      options,
+    )
+  } catch (error) {
+    console.error(error)
+  }
 }
 
-fanoutExchange();
-
-
-// amqp.connect("amqp://localhost", function (error0, connection) {
-//   if (error0) {
-//     throw error0;
-//   }
-//   connection.createChannel(async (error1, channel) => {
-//     if (error1) {
-//       throw error1;
-//     }
-
-//     const exchangeKey = "mysabay.user.purchase.video";
-//     const exchangeType = "fanout";
-//     const routingKey = "";
-//     const options = {};
-//     const payload = {
-//         vehicleType: "car",
-//         numberOfPassenger: 3,
-//     };
-
-//     await channel.assertExchange(exchangeKey, exchangeType, options);
-//     channel.publish(
-//         exchangeKey,
-//         routingKey,
-//         Buffer.from(JSON.stringify(payload)),
-//         options
-//     );
-    
-//     console.log(" [x] Sent ===================================>");
-//   });
-//   setTimeout(function () {
-//     connection.close();
-//     process.exit(0);
-//   }, 500);
-// });
+fanoutExchange()
